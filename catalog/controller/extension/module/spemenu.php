cb32a36745bc0078162844bcbae2594d3321bde4
LevikV <alex@kplus79.ru>
2021-08-04 20:18:23 +1000

    Делаю фронт модуля Spe Menu.

--- catalog/controller/extension/module/spemenu.php
+++ catalog/controller/extension/module/spemenu.php
@@ -3,16 +3,23 @@ class ControllerExtensionModuleSpemenu extends Controller {
 	public function index() {
 
 		$this->load->model('extension/module/spemenu');
-
+        $this->load->model('tool/image');
 
 
         $menu_data = $this->model_extension_module_spemenu->getTreeItems();
         $spemenu_tree = $this->model_extension_module_spemenu->getMapTree($menu_data);
 
+        $categories = $this->load->controller('extension/module/tree_cats/getTreeCats');
+
+        foreach ($categories as &$category) {
+            if ($category['image'] != '') {
+                $category['image'] = $this->model_tool_image->resize($category['image'], 80, 80);
+            }
+        }
 
         foreach ($spemenu_tree as &$item) {
             if ($item['type_item'] === 'type-cat') {
-                $item['children']  = $this->load->controller('extension/module/tree_cats/getTreeCats');
+                $item['children']  = $categories;
             }
         }
 
